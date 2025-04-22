062f8be10ebfe5ba5675c830f6b0b5ba8d42ee69
laIvSxR <ma.gallardor@duocuc.cl>
2025-04-22 13:52:16 -0400

    initial commit

---
+++ src/test/java/com/matias/tallerapirest/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.tallerapirest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
